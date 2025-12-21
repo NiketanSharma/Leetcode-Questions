6a3798325ba20c80ca7fe1f86c07bc72f2c8db40
Niketan Sharma <149008719+NiketanSharma@users.noreply.github.com>
2025-12-21 23:50:13 +0530

    Update 88.Merge Sorted Array.cpp

--- Top Interview 150/Arrays/88.Merge Sorted Array.cpp
+++ Top Interview 150/Arrays/88.Merge Sorted Array.cpp
@@ -7,11 +7,11 @@ using namespace std;
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int p1 = m - 1; // Pointer for the last valid element in nums1
-        int p2 = n - 1; // Pointer for the last element in nums2
-        int p = m + n - 1; // Pointer for the final position in nums1
+        int p1=m-1; // Pointer for the last valid element in nums1
+        int p2=n-1; // Pointer for the last element in nums2
+        int p=m+n-1; // Pointer for the final position in nums1
         // Merge nums1 and nums2 from the back
-        while (p1 >= 0 && p2 >= 0) {
+        while (p1>=0 && p2>=0) {
             if (nums1[p1] > nums2[p2]) {
                 nums1[p] = nums1[p1];
                 p1--;
