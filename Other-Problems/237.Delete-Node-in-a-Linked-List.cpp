0fc6df10f50ac084ec8ffc377358142ad11f280a
Niketan Sharma <149008719+NiketanSharma@users.noreply.github.com>
2026-06-24 11:44:59 +0530

    Reverse a Linked list

--- Other-Problems/237.Delete-Node-in-a-Linked-List.cpp
+++ Other-Problems/237.Delete-Node-in-a-Linked-List.cpp
@@ -1,11 +1,11 @@
 /**
  * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
  */
+ struct ListNode {
+     int val;
+      ListNode *next;
+      ListNode(int x) : val(x), next(nullptr) {}
+  };
 #include<bits/stdc++.h>
 using namespace std;
 class Solution {
