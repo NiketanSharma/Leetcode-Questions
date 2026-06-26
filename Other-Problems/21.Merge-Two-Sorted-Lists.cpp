9193e4c729ec4ed894643517bee6f248950b612e
Niketan Sharma <149008719+NiketanSharma@users.noreply.github.com>
2026-06-26 10:43:08 +0530

    Create 21.Merge-Two-Sorted-Lists.cpp

---
+++ Other-Problems/21.Merge-Two-Sorted-Lists.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+/**
+ * Definition for singly-linked list.
+ */
+  struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+  };
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* dummy=new ListNode(-1);
+        ListNode* temp=dummy;
+        while(list1!=nullptr && list2!=nullptr){
+            if(list1->val<=list2->val){
+                temp->next=list1;
+                list1=list1->next;
+            }else{
+                temp->next=list2;
+                list2=list2->next;
+            }
+            temp=temp->next;
+        }
+        if(list1!=nullptr)temp->next=list1;
+        else temp->next=list2;
+        return dummy->next;
+    }
+};
+// Approach:
+// This solution merges two sorted linked lists into one sorted linked list.
+// It uses a dummy node to simplify the merging process. A pointer `temp` is used to build the new list by comparing the values of the nodes in `list1` and `list2`.
+// The smaller value node is appended to the merged list, and the corresponding pointer is moved forward
